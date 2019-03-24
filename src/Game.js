eff99d5e332e0263e1c500d5640e0f91e771a038
Julia <julia.yan@hotmail.com>
2019-03-24 11:58:45 -0400

    Completed game page styling (not the actual game)

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 import { subscribeToTimer, buttonClicked, newPlayer } from "./api";
 import { GAME_HEIGHT, GAME_WIDTH } from "./config";
 import Race from "./scenes/race";
+import "./style/game.css";
 export class Game extends React.Component {
   componentDidMount() {
     const config = {
