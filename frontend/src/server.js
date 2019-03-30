53d3f91728c8e03f36994e5033598e7ff0be1e3b
Julia <julia.yan@hotmail.com>
2019-03-30 16:01:01 -0400

    removing listen

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -96,12 +96,12 @@ io.on("connection", client => {
   // });
 });
 
-const port = 8081;
-//const port = 'https://forumla0.herokuapp.com/game';
-io.listen(port, function(err) {
-  if (err) throw err;
-  console.log("listening on port 8081");
-});
+// const port = 8081;
+// //const port = 'https://forumla0.herokuapp.com/game';
+// io.listen(port, function(err) {
+//   if (err) throw err;
+//   console.log("listening on port 8081");
+// });
 // socket.on('disconnect', state => {
 //   delete players[socket.id]
 //   io.emit('update-players', players)
