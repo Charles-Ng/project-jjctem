ae2121561c452c21adffb6bdc7ee1b8d500e1c32
Julia <julia.yan@hotmail.com>
2019-03-30 17:59:51 -0400

    use host

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import Phaser from "phaser";
 // const socket = openSocket("https://formula0.julesyan.com", {path:'/socket', secure: true, reconnect: true, rejectUnauthorized: false});
 // const socket = openSocket.connect("https://formula0.julesyan.com", { path:'/socket',secure: true, reconnect: true, rejectUnauthorized: false });
 // const  socket = openSocket("https://formula0.julesyan.com", {path:'/socket'});
-const socket = openSocket.connect("https://159.203.38.244:8081", {path: "/", rejectUnauthorized: false});
+const socket = openSocket.connect("https://formula0.julesyan.com:8081", {path: "/", rejectUnauthorized: false});
 
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
