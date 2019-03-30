f523c79b4eed5eee655aea1ddb5d0d79abb3cc1d
Julia <julia.yan@hotmail.com>
2019-03-30 14:56:28 -0400

    Logging errors

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -100,9 +100,9 @@ const port = 8081;
 //const port = 'https://forumla0.herokuapp.com/game';
 io.listen(port, function(err) {
   if (err) throw err;
+  console.log(err);
+  console.log("listening on port 8081");
 });
-
-console.log("listening on port 8081");
 // socket.on('disconnect', state => {
 //   delete players[socket.id]
 //   io.emit('update-players', players)
