66ef664cd3ddf31173ea09a87ae0583f90ecfacb
Julia <julia.yan@hotmail.com>
2019-03-31 12:45:53 -0400

    remove the log

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -6,7 +6,6 @@ import logo from './logo.PNG';
 export class Root extends React.Component {
     render() {
         return (
-            console.log(this);
             <nav className="navbar navbar-default">
                 <NavLink exact to={"/"}>
                     <img src={logo} alt="Logo" />
