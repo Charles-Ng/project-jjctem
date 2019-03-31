90901a0136ba7e640715bed64429e487c57b2646
Julia <julia.yan@hotmail.com>
2019-03-31 12:42:39 -0400

    temp removal of nav

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -6,9 +6,6 @@ import logo from './logo.PNG';
 export class Root extends React.Component {
     render() {
         console.log(this);
-        // if (this.props.location == undefined || this.props.location.state == undefined) {
-        //   return (
-            // if user didnt signin or singup
             <nav className="navbar navbar-default">
                 <NavLink exact to={"/"}>
                     <img src={logo} alt="Logo" />
@@ -17,17 +14,6 @@ export class Root extends React.Component {
                     <NavLink exact to={"/"} activeClassName="selected"><span>Home</span></NavLink>
                     <NavLink to={"/signin"} activeClassName="selected"><span>Sign In</span></NavLink>
                     <NavLink to={"/signup"} activeClassName="selected"><span>Sign Up</span></NavLink>
-        //         </div>
-        //     </nav>
-        //   );
-        // } else if (this.props.location.state.checkLogin == true) {
-        //     return (
-        //         <nav className="navbar navbar-default">
-        //             <NavLink exact to={"/"}>
-        //                 <img src='/assets/logo.png' alt="Logo" />
-        //             </NavLink>
-        //             <div className="links">
-        //                 <NavLink exact to={"/"} activeClassName="selected"><span>Home</span></NavLink>
                         <NavLink to={"/game"} activeClassName="selected"><span>Play</span></NavLink>
                         <NavLink to={"/logout"} activeClassName="selected"><span>Logout</span></NavLink>
                     </div>
