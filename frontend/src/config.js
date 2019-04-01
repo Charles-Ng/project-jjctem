f0c15f3ef11c5a0bc6c5453eb7b11ddf8461e0ac
Julia <julia.yan@hotmail.com>
2019-03-31 23:45:23 -0400

    Change config for server

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,30 +7,30 @@ module.exports = {
 
 
     // Local config
-    BACKEND_URL:  "http://localhost:8000",
-    BACKEND_PORT: 8000,
-    SOCKET_PORT: 8081,
-    MYSQL_PORT: 8889,
+    // BACKEND_URL:  "http://localhost:8000",
+    // BACKEND_PORT: 8000,
+    // SOCKET_PORT: 8081,
+    // MYSQL_PORT: 8889,
     MYSQL_USER: 'root',
     MYSQL_PASS: 'root',
-    APP_PORT: 3000,
-    SERVER_TYPE: "http",
-    SOCKET_SERVER: function(http, app, fs) {return http.Server(app);},
-    SOCKET_URL: 'ws://localhost:8081',
+    // APP_PORT: 3000,
+    // SERVER_TYPE: "http",
+    // SOCKET_SERVER: function(http, app, fs) {return http.Server(app);},
+    // SOCKET_URL: 'ws://localhost:8081',
 
 
     // Server config
-    // BACKEND_URL: "https://forumla0.julesyan.com/api",
-    // BACKEND_PORT: 8000,
-    // SOCKET_PORT: 8081,
-    // MYSQL_PORT: 3006,
-    // APP_PORT: 3000,
-    // SERVER_TYPE: "https",
-    // SOCKET_SERVER: function(https, app, fs) {
-    //     return new https.createServer({
-    //         cert: fs.readFileSync('/etc/letsencrypt/live/formula0.julesyan.com/cert.pem'),
-    //         key: fs.readFileSync('/etc/letsencrypt/live/formula0.julesyan.com/privkey.pem')
-    //     });
-    // },
-    // SOCKET_URL: 'wss://formula0.julesyan.com:8081',
+    BACKEND_URL: "https://forumla0.julesyan.com/api",
+    BACKEND_PORT: 8000,
+    SOCKET_PORT: 8081,
+    MYSQL_PORT: 3306,
+    APP_PORT: 3000,
+    SERVER_TYPE: "https",
+    SOCKET_SERVER: function(https, app, fs) {
+        return new https.createServer({
+            cert: fs.readFileSync('/etc/letsencrypt/live/formula0.julesyan.com/cert.pem'),
+            key: fs.readFileSync('/etc/letsencrypt/live/formula0.julesyan.com/privkey.pem')
+        });
+    },
+    SOCKET_URL: 'wss://formula0.julesyan.com:8081',
 }
