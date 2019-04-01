e1bc910ae4e421d18db8cb9bb1ecccd298a58215
Julia <julia.yan@hotmail.com>
2019-03-31 23:59:21 -0400

    adde header

--- backend/app.js
+++ backend/app.js
@@ -45,8 +45,8 @@ app.use(function(req, res, next) {
     // }
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.Header('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
        res.send(200);
    } else {
