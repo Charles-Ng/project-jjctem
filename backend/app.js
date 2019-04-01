409418383e1dbbd990fa536948e867251ef1e14b
Julia <julia.yan@hotmail.com>
2019-03-31 23:57:58 -0400

    allow cookies

--- backend/app.js
+++ backend/app.js
@@ -45,8 +45,8 @@ app.use(function(req, res, next) {
     // }
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
        res.send(200);
    } else {
