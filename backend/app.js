cea60f373ec8312e9cb5831e654790a5a058fc1d
Julia <julia.yan@hotmail.com>
2019-03-31 23:54:09 -0400

    allowed options

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
     //     res.setHeader('Access-Control-Allow-Origin', origin);
     // }
     res.header('Access-Control-Allow-Origin', 'https://forumla0.julesyan.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
